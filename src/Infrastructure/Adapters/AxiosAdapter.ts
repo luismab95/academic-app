7ad0f4c649477e4668d7ec382c1dd6821b555e34
Luis Barragan <luismab95@gmail.com>
2025-04-25 17:30:20 -0500

    feat: Enhance Axios adapter with error handling for public key retrieval and validation; integrate error store for better error management

--- src/Infrastructure/Adapters/AxiosAdapter.ts
+++ src/Infrastructure/Adapters/AxiosAdapter.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import DeviceInfo from 'react-native-device-info';
 import {API_URL} from '@env';
-import {authStore, decryptedData, encryptedData} from '../../Shared';
+import {
+  authStore,
+  decryptedData,
+  encryptedData,
+  errorStore,
+  servicesContainer,
+  validHash,
+} from '../../Shared';
 import {StorageAdapter} from './StorageAdapter';
 import {AuthSession} from '../../Domian';
 
+let originalPayload = {};
 const apiURL = API_URL;
 
 console.info('API_URL', apiURL);
-
 
 const axiosApi = axios.create({
   baseURL: apiURL,
@@ -23,7 +30,7 @@ axiosApi.interceptors.request.use(async config => {
     !config.url?.includes('/public-key')
   ) {
     const publickeyServer = await StorageAdapter.getItem('publicKey-server');
-
+    originalPayload = config.data;
     if (config.data !== undefined)
       config.data = {data: await encryptedData(config.data, publickeyServer!)};
   }
@@ -44,6 +51,7 @@ axiosApi.interceptors.response.use(
       !response.config.url?.includes('/device') &&
       !response.config.url?.includes('/public-key')
     ) {
+      originalPayload = {};
       const decrypt = await decryptedData(response.data.data);
       response.data = {...response.data, data: decrypt};
     }
@@ -61,9 +69,43 @@ axiosApi.interceptors.response.use(
     return response;
   },
   async error => {
+    console.log('Error en la respuesta de Axios:', error.config);
+
+    const originalRequest = error.config;
+
     if (error.response && error.response.status === 401) {
       const {logout} = authStore.getState();
       await logout!();
+    }
+
+    if (error.response && error.response.status === 403) {
+      if (!originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          const response = await servicesContainer.auth.getPublicKey();
+
+          if (response === null) {
+            await StorageAdapter.removeItem('publicKey-server');
+            throw new Error('No se pudo obtener la llave pública');
+          }
+
+          if (!validHash(response.data.publicKey, response.data.sha256Hash)) {
+            errorStore.setState({
+              message: 'No se pudo verificar la llave pública',
+            });
+            throw new Error('No se pudo verificar la llave pública');
+          }
+          await StorageAdapter.setItem(
+            'publicKey-server',
+            response.data.publicKey,
+          );
+
+          originalRequest.data = originalPayload;
+          return axiosApi(originalRequest);
+        } catch (refreshError) {
+          return Promise.reject(refreshError);
+        }
+      }
     }
     return Promise.reject(
       new Error(error.response?.data?.message || error.message),
